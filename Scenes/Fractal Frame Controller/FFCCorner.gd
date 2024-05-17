31c0529656174256ba1db441641d8d9b80ef7ced
toucam87 <49007994+toucam87@users.noreply.github.com>
2024-05-17 11:15:34 -0600

    v0.1.1.2 frame controller resizeable
    finished working on the drag and drop ui mechanic for the frame controller, it now rotates and scales in one smooth motion. it is still not controlling the actual fractal frames

--- Scenes/Fractal Frame Controller/FFCCorner.gd
+++ Scenes/Fractal Frame Controller/FFCCorner.gd
@@ -1,19 +1,29 @@
 extends Panel
 
+#Corner is a global enum in godot
+@export var corner_enum : Corner
 @onready var fractal_frame_controller = $"../.." as FractalFrameController
 
-enum Corner {TOPLEFT, TOPRIGHT, BOTLEFT, BOTRIGHT}
+@export var is_dragging = false
 
-var frame_start_position : Vector2
-var frame_start_angle : float
-var frame_start_height : float
-var frame_start_diagonal : Vector2
-var is_dragging = false
-var start_drag_position : Vector2
-var current_drag_position : Vector2
-var drag_position_from_pivot : Vector2
-var pivot_point := Vector2(0,0) 
+@export var frame_start_position : Vector2
+@export var frame_start_angle : float
+@export var frame_start_size : Vector2
 
+@export var corner_start_local_position : Vector2
+@export var frame_start_local_diagonal : Vector2
+@export var local_pivot_point := Vector2(0,0) 
+
+@export var start_drag_position : Vector2
+@export var canvas_mouse_pos : Vector2
+@export var drag_canvas_translation : Vector2
+@export var drag_local_translation : Vector2
+@export var drag_local_position_from_pivot : Vector2
+
+@export var translation_vector : Vector2
+
+@export var rotation_angle : float
+@export var scale_factor : float
 
 func _gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left click"):
@@ -21,8 +31,10 @@ func _gui_input(event: InputEvent) -> void:
 		start_drag_position = Mouse.mouse_pos
 		frame_start_position = fractal_frame_controller.position
 		frame_start_angle = fractal_frame_controller.rotation
-		frame_start_height = fractal_frame_controller.size.y
-		calculate_diagonal()
+		frame_start_size = fractal_frame_controller.size
+		calculate_corner_local_position()
+		calculate_local_pivot_point()
+		calculate_local_diagonal()
 		
 	if event.is_action_released("left click"):
 		is_dragging = false
@@ -32,41 +44,103 @@ func _gui_input(event: InputEvent) -> void:
 
 func _process(delta: float) -> void:
 	if is_dragging:
-		current_drag_position = Mouse.mouse_pos
+		canvas_mouse_pos = Mouse.mouse_pos
 		#measure the vector from pivot to mouse position 
-		calculate_drag_position_from_pivot()
-		resize_and_rotate_frame(pivot_point, calculate_rotation(), calculate_scale_factor())
+		calculate_drag_local_position_from_pivot()
+		calculate_rotation()
+		calculate_scale_factor()
+		resize_and_rotate_frame(local_pivot_point, rotation_angle, scale_factor)
 
 
-#TODO calculate from other pivots
-func calculate_drag_position_from_pivot():
-	drag_position_from_pivot = current_drag_position - frame_start_position
+func calculate_corner_local_position():
+	match corner_enum:
+		CORNER_TOP_LEFT:
+			corner_start_local_position = Vector2(0,0)
+		CORNER_TOP_RIGHT:
+			corner_start_local_position = Vector2(fractal_frame_controller.size.x,0)
+		CORNER_BOTTOM_LEFT:
+			corner_start_local_position = Vector2(0,fractal_frame_controller.size.y)
+		CORNER_BOTTOM_RIGHT:
+			corner_start_local_position = Vector2(fractal_frame_controller.size.x,fractal_frame_controller.size.y)
+	#print("corner " + str(corner_start_local_position))
+
+func calculate_local_pivot_point():
+	match corner_enum:
+		CORNER_TOP_LEFT:
+			local_pivot_point = Vector2(fractal_frame_controller.size.x,fractal_frame_controller.size.y)
+		CORNER_TOP_RIGHT:
+			local_pivot_point = Vector2(0,fractal_frame_controller.size.y)
+		CORNER_BOTTOM_LEFT:
+			local_pivot_point = Vector2(fractal_frame_controller.size.x,0)
+		CORNER_BOTTOM_RIGHT:
+			local_pivot_point = Vector2(0,0)
+	#print("pivot " + str(local_pivot_point))
+
+#FIXTHIS 
+func calculate_drag_local_position_from_pivot():
+	drag_canvas_translation = canvas_mouse_pos - start_drag_position
+	drag_local_translation = drag_canvas_translation.rotated(- frame_start_angle)
+	drag_local_position_from_pivot = frame_start_local_diagonal + drag_local_translation
 
 
-#TODO calculate diagonal for different pivots
-func calculate_diagonal():
-	frame_start_diagonal = Vector2(fractal_frame_controller.size.x,fractal_frame_controller.size.y).rotated(frame_start_angle)
+
+func calculate_local_diagonal():
+	frame_start_local_diagonal = (corner_start_local_position - local_pivot_point) 
+
 	
 	
-func calculate_rotation() -> float:
-	var rotation_angle = frame_start_diagonal.angle_to(drag_position_from_pivot) 
-	return rotation_angle
+func calculate_rotation():
+	rotation_angle = frame_start_local_diagonal.angle_to(drag_local_position_from_pivot) 
 
 
-func calculate_scale_factor() -> float:
-	var scale_factor = drag_position_from_pivot.length() / frame_start_diagonal.length()
-	return scale_factor
 
 
-func resize_and_rotate_frame(_pivot, _angle, _scale):
-	#rotate frame #TODO take pivot into account
+func calculate_scale_factor():
+	scale_factor = drag_local_position_from_pivot.length() / frame_start_local_diagonal.length()
+
+
+
+func resize_and_rotate_frame(_pivot: Vector2, _angle : float, _scale: float):
+	#rotate frame
 	fractal_frame_controller.rotation = frame_start_angle + _angle
+	#translate frame according to which corner was pulled // this translation only takes into account rotation
+	#holy shit these formulas were hard to find, but good trig and geometry skills... ugh.
+	var new_fractal_frame_position = fractal_frame_controller.position
+	translation_vector = Vector2(0,0)
+	match corner_enum:
+		CORNER_TOP_LEFT:
+			translation_vector = (-frame_start_local_diagonal + frame_start_local_diagonal.rotated(_angle)).rotated(frame_start_angle)
+			new_fractal_frame_position = frame_start_position + translation_vector
+			fractal_frame_controller.position = new_fractal_frame_position
+		CORNER_TOP_RIGHT:
+			translation_vector = Vector2(_pivot.length() *(sin(_angle)), _pivot.length() * (1.0 - cos(_angle))).rotated(frame_start_angle)
+			new_fractal_frame_position = frame_start_position + translation_vector
+			fractal_frame_controller.position = new_fractal_frame_position
+		CORNER_BOTTOM_LEFT:
+			translation_vector = Vector2(_pivot.length() *(1.0 - cos(_angle)), - _pivot.length() * sin(_angle)).rotated(frame_start_angle)
+			new_fractal_frame_position = frame_start_position + translation_vector
+			fractal_frame_controller.position = new_fractal_frame_position
+		CORNER_BOTTOM_RIGHT:
+			pass
+		
 	#scale frame
-	fractal_frame_controller.size.y = frame_start_height * _scale 
-	fractal_frame_controller.size.x = fractal_frame_controller.size.y * 16.0 / 9.0
+	fractal_frame_controller.size = frame_start_size * _scale 
+	#translation due to resizing
+	translation_vector = Vector2(0,0)
+	match corner_enum:
+		CORNER_TOP_LEFT:
+			translation_vector = Vector2(_pivot.x * (1 - _scale), _pivot.y * (1 - _scale)).rotated(_angle).rotated(frame_start_angle)
+			new_fractal_frame_position += translation_vector
+			fractal_frame_controller.position = new_fractal_frame_position
+		CORNER_TOP_RIGHT:
+			translation_vector = Vector2(0, _pivot.y * (1 - _scale)).rotated(_angle).rotated(frame_start_angle)
+			new_fractal_frame_position += + translation_vector
+			fractal_frame_controller.position = new_fractal_frame_position
+		CORNER_BOTTOM_LEFT:
+			translation_vector = Vector2(_pivot.x * (1 - _scale), 0).rotated(_angle).rotated(frame_start_angle)
+			new_fractal_frame_position += translation_vector
+			fractal_frame_controller.position = new_fractal_frame_position
+		CORNER_BOTTOM_RIGHT:
+			pass
 	
-	
-	#print(_pivot)
-	print(_scale)
-
 	
--- Scenes/Fractal Frame Controller/FractalFrameController.gd
+++ Scenes/Fractal Frame Controller/FractalFrameController.gd
@@ -1,6 +1,8 @@
 extends Control
 class_name FractalFrameController
 
+#TODO remove this when everything is working as expected
+@export var angles_in_rad : float
 
 func _ready() -> void:
 	resize(450)
@@ -9,3 +11,6 @@ func resize(_height):
 	size.y = _height
 	size.x = _height * 16 / 9
 
+func _process(delta: float) -> void:
+	#TODO remove this when everything is working as expected
+	angles_in_rad = rotation
